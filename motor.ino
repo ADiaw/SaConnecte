fa1de8caa131bb3f3b6c91683f54e16f58d71af3
Shuyun <shuyun.xie@outlook.com>
2016-05-09 14:06:44 +0200

    code moteur avec les comments

---
+++ motor.ino
@@ -0,0 +1,36 @@
+int motorPin1 = 7;//Numero du porte
+int motorPin2 = 6;
+int motorPin3 = 5;
+int motorPin4 = 4;
+
+int delayTime = 9;//temps de delay
+
+void setup() {   //initiation des pins
+pinMode(motorPin1, OUTPUT);
+pinMode(motorPin2, OUTPUT);
+pinMode(motorPin3, OUTPUT);
+pinMode(motorPin4, OUTPUT);
+}
+
+void loop() { //alimentation du pin un par un
+digitalWrite(motorPin1, HIGH);
+digitalWrite(motorPin2, LOW);
+digitalWrite(motorPin3, LOW);
+digitalWrite(motorPin4, LOW);
+delay(delayTime);
+digitalWrite(motorPin1, LOW);
+digitalWrite(motorPin2, HIGH);
+digitalWrite(motorPin3, LOW);
+digitalWrite(motorPin4, LOW);
+delay(delayTime);
+digitalWrite(motorPin1, LOW);
+digitalWrite(motorPin2, LOW);
+digitalWrite(motorPin3, HIGH);
+digitalWrite(motorPin4, LOW);
+delay(delayTime);
+digitalWrite(motorPin1, LOW);
+digitalWrite(motorPin2, LOW);
+digitalWrite(motorPin3, LOW);
+digitalWrite(motorPin4, HIGH);
+delay(delayTime);
+}
