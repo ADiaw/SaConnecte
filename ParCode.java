dbc5c0231b2e25f13cb071c659efc12b9be3d0c2
thaisgasta <thais.gastaldi@hotmail.fr>
2016-05-11 11:50:30 +0200

    Update ParCode.java

--- ParCode.java
+++ ParCode.java
@@ -14,6 +14,22 @@ public class ParCode extends AppCompatActivity {
     }
 
     public void buttonOpen(View view){
+        //à tester c'est ce qui a fait planter mon ordi, le but serait d'ouvrir un layout en alertdialog. Là j'en ai pris un au pif mais le mieux serait de réussir à faire pareil avec le drawable success ou error
+        public void buttonOpen(View view){
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(ParCode.this);
+        a_builder.setCancelable(false).setNegativeButton("Soit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = a_builder.create();
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.empreintecontent,null);
+        dialog.setView(dialogLayout);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.show();
 
     }
 
