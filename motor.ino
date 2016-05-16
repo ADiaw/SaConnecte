013e2b4003582a605f2c3aac34386c410cdfe0a4
Shuyun <shuyun.xie@outlook.com>
2016-05-16 22:50:04 +0200

    nb d'etape a changer

--- motor.ino
+++ motor.ino
@@ -1,36 +1,40 @@
-int motorPin1 = 7;//Numero du porte
-int motorPin2 = 6;
-int motorPin3 = 5;
-int motorPin4 = 4;
+int motorPin1 = 10;//Numero du porte
+int motorPin2 = 11;
+int motorPin3 = 12;
+int motorPin4 = 13;
 
-int delayTime = 9;//temps de delay
+int delayTime = 3;
 
-void setup() {   //initiation des pins
-pinMode(motorPin1, OUTPUT);
-pinMode(motorPin2, OUTPUT);
-pinMode(motorPin3, OUTPUT);
-pinMode(motorPin4, OUTPUT);
+void setup() {                 //initiation des pins
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+// nb d'etape a changer
+  for(int etape = 0; etape < 1024;etape ++){   //nb d'etape pour tourner 90 degres
+    
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    delay(delayTime);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    delay(delayTime);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    delay(delayTime);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    delay(delayTime);
+    }
+}
+void loop() {                 
 }
 
-void loop() { //alimentation du pin un par un
-digitalWrite(motorPin1, HIGH);
-digitalWrite(motorPin2, LOW);
-digitalWrite(motorPin3, LOW);
-digitalWrite(motorPin4, LOW);
-delay(delayTime);
-digitalWrite(motorPin1, LOW);
-digitalWrite(motorPin2, HIGH);
-digitalWrite(motorPin3, LOW);
-digitalWrite(motorPin4, LOW);
-delay(delayTime);
-digitalWrite(motorPin1, LOW);
-digitalWrite(motorPin2, LOW);
-digitalWrite(motorPin3, HIGH);
-digitalWrite(motorPin4, LOW);
-delay(delayTime);
-digitalWrite(motorPin1, LOW);
-digitalWrite(motorPin2, LOW);
-digitalWrite(motorPin3, LOW);
-digitalWrite(motorPin4, HIGH);
-delay(delayTime);
-}
